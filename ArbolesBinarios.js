c53d3324f8f7f8fcd8e5f97d4603391f21770544
jorgelbp8 <jbriseno@ucol.mx>
2021-06-21 23:01:35 -0500

    Se guardo la segunfa

--- ArbolesBinarios.js
+++ ArbolesBinarios.js
@@ -1,10 +1,35 @@
-const chalk = require('chalk');
-console.log(chalk.green("Hola Profe"));
+//const chalk = require('chalk');
 
-class ArbolBinario
+let cosas = new Arbolbinario();
+
+let producto1 = new productos(1,"Nissan sentra",280000,2);
+cosas.agregar(producto1);
+
+let producto2 = new productos(2,"Nissan Juke",405000,4);
+cosas.agregar(producto2);
+
+let producto3 = new productos(3,"Nissan 350z",235000,6);
+cosas.agregar(producto3);
+
+let producto4 = new productos(4,"Nissan 240sx",170000,3);
+cosas.agregar(producto4);
+
+let producto5 = new productos(5,"Nissan 240z",150000,7);
+cosas.agregar(producto5);
+
+let producto6 = new productos(6,"Nissan GTR",2500000,8);
+cosas.agregar(producto6)
+
+
+
+class productos
 {
-    constructor ()
+    constructor (id,nombre,precio,cantidad)
     {
+        this.id=id;
+        this.nombre=nombre;
+        this.precio=precio;
+        this.cantidad=cantidad;
         this.raiz=null;
         this.hijoIzq=null;
         this.hijoDer=null;
